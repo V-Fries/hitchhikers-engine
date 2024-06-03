ef3abcb94f231e459086f985e2c827e5ea48737e
V-Fries <vincentfries@proton.me>
2024-06-03 15:46:25 +0200

    feat: Added queue families checks when evaluating device suitability

--- src/Vulkan/physical_device.rs
+++ src/Vulkan/physical_device.rs
@@ -3,11 +3,17 @@ use super::errors::NoSuitablePhysicalDevice;
 use ash::vk;
 use anyhow::Result;
 
-type DeviceScore = u32;
+#[derive(Ord, Eq, PartialEq, PartialOrd)]
+struct DeviceScore(u32);
 
 struct ScoredDevice {
     device: vk::PhysicalDevice,
     score: DeviceScore,
+}
+
+#[derive(Default)]
+struct QueueFamilies {
+    graphics: Option<()>,
 }
 
 pub fn get_physical_device(instance: &ash::Instance) -> Result<vk::PhysicalDevice> {
@@ -26,7 +32,7 @@ fn filter_map_physical_device(instance: &ash::Instance,
     let device_properties = unsafe { instance.get_physical_device_properties(device) };
     let device_features = unsafe { instance.get_physical_device_features(device) };
 
-    if !is_device_suitable(device_properties, device_features) {
+    if !is_device_suitable(instance, device) {
         return None;
     }
 
@@ -35,22 +41,32 @@ fn filter_map_physical_device(instance: &ash::Instance,
     Some(ScoredDevice { device, score })
 }
 
-fn is_device_suitable(_device_properties: vk::PhysicalDeviceProperties,
-                      _device_features: vk::PhysicalDeviceFeatures) -> bool {
-    // TODO evaluate device suitability (see example below)
-    // if device_features.geometry_shader == 0 {
-    //     return false;
-    // }
-    true
+fn is_device_suitable(instance: &ash::Instance, device: vk::PhysicalDevice) -> bool {
+    find_queue_families(instance, device)
+        .graphics
+        .is_some()
 }
 
 fn score_device(device_properties: vk::PhysicalDeviceProperties,
                 _device_features: vk::PhysicalDeviceFeatures) -> DeviceScore {
-    let mut score: DeviceScore = 0;
+    let mut score = DeviceScore(0);
 
     if device_properties.device_type == vk::PhysicalDeviceType::DISCRETE_GPU {
-        score += 1000;
+        score.0 += 1000;
     }
 
     score
+}
+
+fn find_queue_families(instance: &ash::Instance,
+                       device: vk::PhysicalDevice) -> QueueFamilies {
+    let queue_families = unsafe { instance.get_physical_device_queue_family_properties(device) };
+    let mut result = QueueFamilies::default();
+
+    for queue_family in queue_families.into_iter() {
+        if queue_family.queue_flags & vk::QueueFlags::GRAPHICS != vk::QueueFlags::default() {
+            result.graphics = Some(());
+        }
+    }
+    result
 }

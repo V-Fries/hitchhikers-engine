0cc387bd68898c43311dc22637608a1201b34cb1
V-Fries <vincentfries@proton.me>
2024-08-16 14:57:26 +0200

    chores: Deleted old vulkan renderer files

--- src/vulkan_renderer_old/device/mod.rs
+++
@@ -1,9 +0,0 @@
-#[allow(clippy::module_inception)]
-mod device;
-mod physical_device;
-
-pub use device::create_device;
-pub use device::create_device_queue;
-pub use device::Queues;
-pub use physical_device::QueueFamilies;
-pub use physical_device::pick_physical_device;
--- src/vulkan_renderer_old/graphics_pipeline/vertex_input.rs
+++
@@ -1,5 +0,0 @@
-use ash::vk;
-
-pub fn vertex_input_state_create_info<'a>() -> vk::PipelineVertexInputStateCreateInfo<'a> {
-    vk::PipelineVertexInputStateCreateInfo::default()
-}
